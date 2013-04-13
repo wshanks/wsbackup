87e98db681df56abf63ae8ebd8da7d8858f45b2e
Will S <willsALMANJ@server.fake>
2013-04-13 16:01:15 -0400

    Make encryption folder names customizable

--- encryption/encode_vault.sh
+++ encryption/encode_vault.sh
@@ -4,18 +4,25 @@
 # unencrypted directory, and then unmounts it.
 
 # This script assumes that you have three directories inside of the same parent: plain,
-# enc, and tmp.  plain should have all the files to be encrypted.  enc will hold the
-# encrypted versions of the files.  tmp is a temporary mount point.  This script should be
-# saved in the plain directory, as should the .xml file with the encryption key (specify
-# the name below in the XML variable).
+# enc, and tmp.  plain (can be named anything) should have all the files to be encrypted
+# and this script.  enc (name can be set below) will hold the encrypted versions of the
+# files.  tmp (name can be set below) is a temporary mount point.  The .xml file with
+# the encryption key should be in the parent directory of plain, enc, and tmp (specify
+# the name below in the XML variable, or specify a path relative to that directory).
 
 # It is possible to pass in a password with the -p flag.  This option is useful for 
 # calling this script from another script, but don't use it in a setting where the input
 # is logged since then the password will be viewable.
 
-# .encfs6.xml file name (does not have to be .encfs6.xml)
+# User settings
+# .encfs6.xml file name
 XML=".encfs6.xml"
+# Path to encfs (AppleScript doesn't search outside /usr/bin).
 ENCFS="/usr/local/bin/encfs"
+# Temporary mount directory
+TMP="tmp"
+# Encrypted directory
+ENC="enc"
 
 while getopts ":p:" OPTIONS; do
   case $OPTIONS in
@@ -27,27 +34,30 @@ while getopts ":p:" OPTIONS; do
   esac
 done
 
-# Dir is directory containing this script
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+# DIR is directory containing this script
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# PARENT is the parent directory of DIR
+PARENT="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
 if [ $PASS ]
 then 
-	echo $PASS | ENCFS6_CONFIG="${DIR}/plain/${XML}" \
-		"${ENCFS}" -S "${DIR}/enc/" "${DIR}/tmp/" -o volname="tmp"
+	echo $PASS | ENCFS6_CONFIG="${PARENT}/${XML}" \
+		"${ENCFS}" -S "${PARENT}/${ENC}/" "${PARENT}/${TMP}/" -o volname="tmp"
 else
-	ENCFS6_CONFIG="${DIR}/plain/${XML}" \
-		"${ENCFS}" "${DIR}/enc/" "${DIR}/tmp/" -- -o volname="tmp"
+	ENCFS6_CONFIG="${PARENT}/${XML}" \
+		"${ENCFS}" "${PARENT}/${ENC}/" "${PARENT}/${TMP}/" -- -o volname="tmp"
 fi
 
 for I in 1 2 3 4 5 6 7 8 9 10
 do
 	# Make sure that the drive mounted before copying files in
-	if ! mount | grep encfs | grep "${DIR}/tmp" >/dev/null;
+	if ! mount | grep encfs | grep "${PARENT}/${TMP}" >/dev/null;
 	then
 		# Not mounted yet
+		:
 	else
 		# On OSX use umount; on Linux use fusermount -u
-		rsync -av --delete "${DIR}/plain/" "${DIR}/tmp" && umount "${DIR}/tmp"
+		rsync -av --delete "${DIR}/" "${PARENT}/${TMP}" && umount "${PARENT}/${TMP}"
 		break
 	fi
 	sleep 2
